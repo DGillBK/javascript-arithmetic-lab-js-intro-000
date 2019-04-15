23ad25a3fdd18976417e81260728e81fe80c5794
DG <d.gill.design@gmail.com>
2019-04-15 16:53:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b){
+  return (a+b)
+}
+add(5,4)
+
+function subtract(a,b){
+  return (a-b)
+}
+subtract(5,4)
+
+function multiply(a,b){
+  return (a*b)
+}
+multiply(5,4)
+
+function divide(a,b){
+  return (a/b)
+}
+divide(10,2)
+
+function inc(n){
+  return n+=
+}
+inc(4)
+
+function dec(n) {
+  return n-=
+}
+dec(4)
